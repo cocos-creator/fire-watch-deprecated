157e8a794f3776e2aa8bfe812c63412927343f55
Johnny Wu <KarasAya@gmail.com>
2015-01-05 19:25:46 +0800

    simple test

--- index.js
+++ index.js
@@ -1,43 +1,183 @@
+var Fs = require('fire-fs');
 var Path = require('fire-path');
 var PathWatcher = require('pathwatcher');
 var Gulp = require('gulp');
 var EventStream = require ('event-stream');
+var Util = require ('util');
+var Events = require ('events');
 
-var files = {};
-var changes = {};
-
-function _watch () {
+function _watch ( watcher ) {
     return EventStream.through(function (file) {
-        if ( file.stat.isDirectory() ) {
-            files[file.path] = [];
+        var parent = watcher.files[Path.dirname(file.path)];
+        parent.children.push( file.path );
 
-            PathWatcher.watch( file.path, function ( event, path ) {
-                changes[file.path] = { command: event, path: path };
-            } );
-        }
-        else {
-            var dirpath = Path.dirname(file.path);
-            files[dirpath].push(file);
-        }
+        watcher.files[file.path] = { file: file, children: [] };
+        PathWatcher.watch( file.path, function ( event, path ) {
+            // console.log("DEBUG: %s, %s, %s", event, file.path, path);
+
+            watcher.changes[file.path] = { command: event, path: file.path, relatedPath: path };
+            // TODO: _cooldown(watcher);
+        } );
     });
 }
 
-function start ( root, cb ) {
-    Gulp.src( [root, Path.join(root,"**/*")], { cwd: root, base: root, read: false } )
-        .pipe ( _watch() ).on( "end", function () {
-            if ( cb ) cb ();
+function _cooldown ( watcher ) {
+    clearTimeout(watcher.timer);
+    watcher.timer = setTimeout(function () {
+        _flush(watcher);
+    }, 500);
+}
+
+function _flush ( watcher ) {
+    var sortedChanges = [];
+    for ( var k in watcher.changes ) {
+        sortedChanges.push(watcher.changes[k]);
+    }
+    watcher.changes = {};
+
+    //
+    sortedChanges.sort( function ( a, b ) {
+        return a.path.localeCompare(b.path);
+    });
+    var results = _computeResults( watcher.files, sortedChanges );
+
+    //
+    watcher.emit( 'changed', results );
+
+}
+
+function _computeResults ( files, changes ) {
+    function getFiles ( path ) {
+        if ( !Fs.existsSync(path) )
+            return [];
+
+        return Fs.readdirSync(path)
+        .filter( function ( name ) {
+            return name[0] !== ".";
+        })
+        .map( function ( name ) {
+            return Path.join( path, name );
         })
         ;
+    }
+    function fastRemove ( array, nth, len ) {
+        array[nth] = array[len-1];
+    }
+
+    var results = {};
+
+    for ( var i = 0; i < changes.length; ++i ) {
+        var info = changes[i];
+        var fileInfo = files[info.path];
+
+        if ( !fileInfo ) {
+            results.push( { command: 'new', path: info.path } );
+            continue;
+        }
+
+        // get file
+        var file = fileInfo.file;
+
+        // if changed file is folder
+        if ( file.stat.isDirectory() ) {
+            var oldFiles = fileInfo.children.slice();
+            var newFiles = getFiles( file.path );
+            var sameFiles = [];
+            var oldLen = oldFiles.length;
+            var newLen = newFiles.length;
+            var j, jj, path, stat;
+
+            for ( j = 0; j < newLen; ++j ) {
+                for ( jj = 0; jj < oldLen; ++jj ) {
+                    var newpath = newFiles[j];
+                    var oldpath = oldFiles[jj];
+
+                    if ( newpath === oldpath ) {
+                        sameFiles.push(newpath);
+
+                        fastRemove( newFiles, j, newLen );
+                        newLen -= 1; --j;
+
+                        fastRemove( oldFiles, jj, oldLen );
+                        oldLen -= 1; --jj;
+
+                        break;
+                    }
+                }
+            }
+
+            for ( j = 0; j < newLen; ++j ) {
+                path = newFiles[j];
+                results[path] = { command: "new", path: path };
+            }
+
+            for ( j = 0; j < oldLen; ++j ) {
+                path = oldFiles[j];
+                results[path] = { command: "delete", path: path };
+            }
+
+            for ( j = 0; j < sameFiles.length; ++j ) {
+                path = sameFiles[j];
+                stat = Fs.statSync(path);
+                if ( files[path].file.stat.mtime.getTime() !== stat.mtime.getTime() ) {
+                    results[path] = { command: "change", path: path };
+                }
+            }
+        }
+        else {
+            if ( info.command === "change" ) {
+                results[info.path] = { command: "change", path: info.path };
+            }
+            else if ( info.command === "rename" ) {
+                results[info.path] = { command: "delete", path: info.path };
+                results[info.relatedPath] = { command: "new", path: info.relatedPath };
+            }
+            else if ( info.command === "delete" ) {
+                results[info.path] = { command: "delete", path: info.path };
+            }
+        }
+    }
+
+    return results;
 }
 
-function stop ( cb ) {
-    PathWatcher.closeAllWatchers();
+function FireWatch () {
+    Events.EventEmitter.call(this);
 
-    // TODO: diff changes and return results
-    cb (changes);
+    this.files = {};
+    this.changes = {};
+    this.timer = null;
 }
 
-module.exports = {
-    start: start,
-    stop: stop
+Util.inherits( FireWatch, Events.EventEmitter );
+
+FireWatch.prototype.stop = function ( cb ) {
+    //
+    setTimeout( function () {
+        PathWatcher.closeAllWatchers();
+        clearTimeout(this.timer);
+
+        _flush(this);
+        this.files = {};
+
+        if ( cb ) cb();
+    }.bind(this), 100 );
 };
+
+FireWatch.start = function ( root, cb ) {
+    var watcher = new FireWatch();
+    watcher.files[Path.dirname(root)] = { file: null, children: [] };
+
+    Gulp.src( [root, Path.join(root,"**/*")], { cwd: root, base: root, read: false } )
+        .pipe ( _watch(watcher) ).on( "end", function () {
+            if ( cb ) cb ();
+
+            // DEBUG
+            // console.log(PathWatcher.getWatchedPaths());
+        })
+        ;
+
+    return watcher;
+};
+
+module.exports = FireWatch;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,158 @@
 var FireWatch = require('../index');
 var Fs = require('fire-fs');
+var Path = require('fire-path');
 
 function reset () {
     Fs.rimrafSync("./test/foobar/");
     Fs.copySync("./test/test-data/", "./test/foobar");
 }
 
+function printResults ( results ) {
+    console.log();
+    console.log("    ---------------- results ----------------");
+    for ( var p in results ) {
+        console.log( "    %s: %s", results[p].command, results[p].path );
+    }
+}
+
+var root = Fs.realpathSync("./test/foobar/");
+
+// TODO:
+// Fs.writeFileSync("./test/foobar/foo/foobar.js", "Hello World!");
+// Fs.writeFileSync("./test/foobar/foo/foo-02/foobar.js", "Hello World!");
+// Fs.writeFileSync("./test/foobar/bar/bar-01/foobar-new.js", "Hello World!");
+
 describe('FireWatch', function () {
-    it('should work for simple case', function ( done ) {
+    it('should work for new file', function ( done ) {
+        this.timeout(10000);
         reset();
 
-        FireWatch.start( Fs.realpathSync("./test/foobar/"), function () {
-            Fs.renameSync("./test/foobar/foo/foo-01/", "./test/foobar/bar/bar-04/");
-            Fs.rimrafSync("./test/foobar/foo/bar-04/");
-            Fs.writeFileSync("./test/foobar/foo/foobar.js", "Hello World!");
-            Fs.writeFileSync("./test/foobar/foo/foo-02/foobar.js", "Hello World!");
+        var watcher = FireWatch.start(root, function () {
+            Fs.writeFileSync(Path.join(root, "foo/foo-01/foobar-new.js"), "Hello World!");
+            Fs.writeFileSync(Path.join(root, "foo/foobar-new.js"), "Hello World!");
+        });
+        watcher.stop( function () { done(); } );
+        watcher.on("changed", function ( results ) {
+            printResults(results);
+            var expectResults = {};
+            var path = "";
 
-            setTimeout( function () {
-                FireWatch.stop( function ( results ) {
-                    console.log( results );
-                    done();
-                });
-            }, 500);
-        } );
+            path = Path.join(root, "foo/foo-01/foobar-new.js");
+            expectResults[path] = { command: "new", path: path };
+
+            path = Path.join(root, "foo/foobar-new.js");
+            expectResults[path] = { command: "new", path: path };
+
+            results.should.eql(expectResults);
+        });
+    });
+
+    it('should work for new folder', function ( done ) {
+        this.timeout(10000);
+        reset();
+
+        var watcher = FireWatch.start( root, function () {
+            Fs.mkdirSync(Path.join(root, "foo/foo-01-new"));
+            Fs.mkdirSync(Path.join(root, "foo-new"));
+        });
+        watcher.stop( function () { done(); } );
+        watcher.on( "changed", function ( results ) {
+            printResults(results);
+
+            var expectResults = {};
+            var path = "";
+
+            path = Path.join(root, "foo/foo-01-new");
+            expectResults[path] = { command: "new", path: path };
+
+            path = Path.join(root, "foo-new");
+            expectResults[path] = { command: "new", path: path };
+
+            results.should.eql(expectResults);
+        });
+    });
+
+    it('should work for delete file', function ( done ) {
+        this.timeout(10000);
+        reset();
+
+        var watcher = FireWatch.start( root, function () {
+            Fs.rimrafSync(Path.join(root, "foo/foobar.js"));
+            Fs.rimrafSync(Path.join(root, "foo/foo-01/foobar.js"));
+        });
+        watcher.stop( function () { done(); } );
+        watcher.on( "changed", function ( results ) {
+            printResults(results);
+
+            var expectResults = {};
+            var path = "";
+
+            path = Path.join(root, "foo/foobar.js");
+            expectResults[path] = { command: "delete", path: path };
+
+            path = Path.join(root, "foo/foo-01/foobar.js");
+            expectResults[path] = { command: "delete", path: path };
+
+            results.should.eql(expectResults);
+        });
+    });
+
+    it('should work for delete folder', function ( done ) {
+        this.timeout(10000);
+        reset();
+
+        var watcher = FireWatch.start( root, function () {
+            Fs.rimrafSync(Path.join(root, "foo/foo-01"));
+            Fs.rimrafSync(Path.join(root, "bar"));
+        });
+        watcher.stop( function () { done(); } );
+        watcher.on( "changed", function ( results ) {
+            printResults(results);
+
+            var expectResults = {};
+            var path = "";
+
+            var list = [
+                Path.join(root,"bar"),
+                Path.join(root,"bar/bar-01"),
+                Path.join(root,"bar/bar-02"),
+                Path.join(root,"bar/bar-03"),
+                Path.join(root,"bar/foobar.js"),
+                Path.join(root,"bar/bar-01/foobar.js"),
+                Path.join(root,"bar/bar-02/foobar.js"),
+                Path.join(root,"bar/bar-03/foobar.js"),
+                Path.join(root,"foo/foo-01"),
+                Path.join(root,"foo/foo-01/foobar.js"),
+            ];
+
+            list.forEach( function ( path ) {
+                expectResults[path] = { command: "delete", path: path };
+            });
+            results.should.eql(expectResults);
+        });
+    });
+
+    it('should work for rename file', function ( done ) {
+        this.timeout(10000);
+        reset();
+
+        var watcher = FireWatch.start( root, function () {
+            Fs.renameSync( Path.join(root, "foo/foobar.js"),
+                           Path.join(root, "foo/foobar-rename.js") );
+
+            Fs.renameSync( Path.join(root, "foo/foo-01/foobar.js"),
+                           Path.join(root, "bar/bar-01/foobar-rename.js") );
+
+            Fs.renameSync( Path.join(root, "foo/foo-02/foobar.js"),
+                           Path.join(root, "bar/bar-02/foobar-rename.js") );
+
+            Fs.renameSync( Path.join(root, "foo/foo-03/foobar.js"),
+                           Path.join(root, "bar/bar-02/foobar-rename.js") );
+        });
+        watcher.stop( function () { done(); } );
+        watcher.on( "changed", function ( results ) {
+            printResults(results);
+            // results.should.eql(expectResults);
+        });
     });
 });
